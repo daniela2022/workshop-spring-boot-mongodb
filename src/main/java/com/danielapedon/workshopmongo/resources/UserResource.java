20af10231b5c887bd3c47ca1edcc5a5002ea0ada
Daniela Pedon <danielapedon3@gmail.com>
2022-02-13 22:23:36 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/danielapedon/workshopmongo/resources/UserResource.java
+++ src/main/java/com/danielapedon/workshopmongo/resources/UserResource.java
@@ -42,6 +42,13 @@ public class UserResource {
         return ResponseEntity.created(uri).build();
     }
 
+    @DeleteMapping (value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+       service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 
 
 
