a8402d05548e912e1b578fd8f1163cc7e65895c7
Daniela Pedon <danielapedon3@gmail.com>
2022-02-14 14:10:54 -0300

    Obtendo um post por id

---
+++ src/main/java/com/danielapedon/workshopmongo/services/PostService.java
@@ -0,0 +1,30 @@
+package com.danielapedon.workshopmongo.services;
+
+import com.danielapedon.workshopmongo.domain.Post;
+import com.danielapedon.workshopmongo.domain.User;
+import com.danielapedon.workshopmongo.dto.UserDTO;
+import com.danielapedon.workshopmongo.repository.PostRepository;
+import com.danielapedon.workshopmongo.repository.UserRepository;
+import com.danielapedon.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+
+
+}
