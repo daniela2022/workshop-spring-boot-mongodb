582312a5a84bea78628751c20fda79e4aeca2de9
Daniela Pedon <danielapedon3@gmail.com>
2022-02-14 12:56:51 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/danielapedon/workshopmongo/resources/UserResource.java
+++ src/main/java/com/danielapedon/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.danielapedon.workshopmongo.resources;
 
+import com.danielapedon.workshopmongo.domain.Post;
 import com.danielapedon.workshopmongo.domain.User;
 import com.danielapedon.workshopmongo.dto.UserDTO;
 import com.danielapedon.workshopmongo.services.UserService;
@@ -54,6 +55,12 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 
 
