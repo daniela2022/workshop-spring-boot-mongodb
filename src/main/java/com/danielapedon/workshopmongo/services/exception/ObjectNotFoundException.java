e29d08110a995c6ba2148949700a9fee5376f9d9
Daniela Pedon <danielapedon3@gmail.com>
2022-02-13 22:03:04 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/danielapedon/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.danielapedon.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+}
